c37038335a6477523771f639fb3d8d9a9eee1607
amin allahham <amin allahham@hypatia.tech>
2023-02-04 00:22:04 -0800

    model usage improvement

--- Train.py
+++ Train.py
@@ -1,13 +1,13 @@
+from tkinter import Image
+
 import numpy as np
 import torch
 import torch.nn as nn
-import torchvision.models as models
-from tkinter import Image
-import torch
-import torch.nn as nn
 import torch.optim as optim
+import torchvision.models as models
 import torchvision.transforms as transforms
 from matplotlib import pyplot as plt
+
 from DatasetLoader import DatasetLoader
 
 
@@ -38,21 +38,17 @@ train_set_size = len(train_set)
 train_dataset_loader = torch.utils.data.DataLoader(dataset = train_set, batch_size = batch_size,shuffle = True)
 
 testing_dataset_loader = torch.utils.data.DataLoader(dataset = testing_set, batch_size = batch_size,shuffle = True)
-
-
-
+       
 model = models.mobilenet_v2(
-    pretrained=True,
+    pretrained=False,
 )
 
 model.classifier[1] = nn.Linear(1280, 2)
 
-
-
 model = model.to(device)
 
 
-loss_function =  nn.MSELoss()
+loss_function =  nn.L1Loss()
 
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
@@ -74,80 +70,63 @@ for epoch in range(num_epochs):
 
     for phase in ['train', 'test']:
         if phase == 'train':
-            print("Training...")
+            model.train()
             for i, (images, labels) in enumerate(train_dataset_loader):
                 images = images.to(device)
                 labels = labels.to(device)
             
-                output = model(images)
+                outputs = model(images)
 
-                predicted = output.data
-                
-            
-                predictedAngles = sinAndCosToDegree(predicted)
+                loss = loss_function(outputs, labels)
 
 
-                labelAngles = sinAndCosToDegree(labels)
-
-                print(
-                    'Predicted: ', predictedAngles,
-                    '\n',
-                    'Labels: ', labelAngles
-
-                )
-
-                delta = abs(predictedAngles - labelAngles) # numpy.ndarray
- 
-                deltaCloseEnough = 0.5
-
-                train_total += labels.size(0)
-
-                train_correct += (delta < deltaCloseEnough).sum().item()
-
-                
-
-                loss = loss_function(predicted, labels)
-                loss.requires_grad = True 
-
-                print('Loss: ', loss)
-
-                losses['train'].append(loss.item())
-                acc['train'].append(train_correct / train_total)
-
-                accuracy = train_correct / train_total
-
-        
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
 
+
+            
+                predictedAngles = sinAndCosToDegree(outputs.data)
+                labelAngles = sinAndCosToDegree(labels)
+
+                closeEnough = 0.5
+
+                train_total += labels.size(0)
+                train_correct += (abs(predictedAngles - labelAngles) < closeEnough).sum().item()
+
+                accuracy = train_correct / train_total
+
+                
                 print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
                     .format(epoch+1, num_epochs, i+1, total_step, loss.item(), accuracy*100))
-
-
              
         else:
-            print("Testing...")
             model.eval()
-            valid_loss = 0.0
-            for i, (images, labels) in enumerate(testing_dataset_loader):
-                images = images.to(device)
-                labels = labels.to(device)
+            with torch.no_grad():
+                for images, labels in testing_dataset_loader:
+                    images = images.to(device)
+                    labels = labels.to(device)
+                    outputs = model(images)
 
-                output = model(images)
-                predicted = output.data
-                predicted = predicted.view(1,-1)
-                
-                labels = labels / 90
-                test_total += labels.size(0)
-                closeEnough = 0.5 / 90
+                    loss = loss_function(outputs, labels)
 
-                test_correct += (abs(predicted - labels) < closeEnough).float().sum().item()
+                    predictedAngles = sinAndCosToDegree(outputs.data)
+                    labelAngles = sinAndCosToDegree(labels)
 
-                delta = abs(predicted - labels).sum().item() / labels.size(0)
+                    closeEnough = 0.5
 
-                labels = labels.view(-1,1)
-               
+                    test_total += labels.size(0)
+                    test_correct += (abs(predictedAngles - labelAngles) < closeEnough).sum().item()
+
+                    accuracy = test_correct / test_total
+
+                    print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
+                        .format(epoch+1, num_epochs, i+1, total_step, loss.item(), accuracy*100))
+            
+            # Save the model checkpoint
+            torch.save(model.state_dict(), 'model.ckpt')
+
+
 
 
 
